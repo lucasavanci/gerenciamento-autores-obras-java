6c146d502f0320e3e06d510106a7d4dca42b1788
Lucas Avanci <lucasavanci98@gmail.com>
2025-02-17 20:07:18 -0300

    Spring Initializer
    Subindo projeto, primeiro commit

---
+++ gerenciamento.autores.obras/src/test/java/com/br/gerenciamento/autores/obras/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gerenciamento.autores.obras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
