0e33b5979a6d9f7d8e786599ea869a1527531cfd
Lucas Avanci <lucasavanci98@gmail.com>
2025-02-19 21:31:14 -0300

    422

--- gerenciamento.autores.obras/src/main/java/com/br/gerenciamento/autores/obras/service/CadastroAutorService.java
+++ gerenciamento.autores.obras/src/main/java/com/br/gerenciamento/autores/obras/service/CadastroAutorService.java
@@ -2,9 +2,14 @@ package com.br.gerenciamento.autores.obras.service;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class CadastroAutorService {
  
     
-    
+
 }
