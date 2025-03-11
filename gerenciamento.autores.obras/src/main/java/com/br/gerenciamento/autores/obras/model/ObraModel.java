99727eaa30a071017e545a5e33dbd77423a67eb6
Lucas Avanci <lucasavanci98@gmail.com>
2025-03-11 19:54:48 -0300

    Update ObraModel.java

--- gerenciamento.autores.obras/src/main/java/com/br/gerenciamento/autores/obras/model/ObraModel.java
+++ gerenciamento.autores.obras/src/main/java/com/br/gerenciamento/autores/obras/model/ObraModel.java
@@ -20,10 +20,50 @@ public class ObraModel {
     @Column(nullable = false)
     private String nomeObra;
     @Column(nullable = false)
-    private String Descricao;
+    private String descricao;
     @Column(nullable = true)
     private LocalDate dataPublicacao;
     @Column(nullable = true)
     private LocalDate dataExposicao;
+
+    public Long getIdObra(){
+        return idObra;
+    }
+
+    public void setIdObra(Long idObra){
+        this.idObra = idObra;
+    }
+
+    public String getNomeObra(){
+        return nomeObra;
+    }
+
+    public void setNomeObra(String nomeObra){
+        this.nomeObra = nomeObra;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataPublicacao(){
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDate dataPublicacao){
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public LocalDate getDataExposicao(){
+        return dataExposicao;
+    }
+
+    public void setDataExposicao(LocalDate dataExposicao){
+        this.dataExposicao = dataExposicao;
+    }
 
 }
